21dfed14d15d849eda5a34c901e37329d5708560
josglenn <josglenn123@gmail.com>
2024-04-24 18:12:21 +0800

    Feat: Add font family nunito sans

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ProfileHeader from "../components/account/profileHeader";
 import Dashboard from "../components/dashboard/dashboard";
-
+import { NunitoSans } from "../components/ui/fonts";
 function DashBoardLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ProfileHeader />
       <Dashboard />
-      <div className="px-10 ">{children}</div>
+      <div className={`px-10 ${NunitoSans.className}`}>{children}</div>
     </>
   );
 }
---
+++ app/components/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Nunito_Sans } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const NunitoSans = Nunito_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
